b7d794ac262ac2607720973b95f5076af19ea425
Vũ Khương Duy <vukhuongduy23@gmail.com>
2020-04-13 09:51:23 +0700

    Add component search

--- src/screens/MessageScreen/MessageScreen.js
+++ src/screens/MessageScreen/MessageScreen.js
@@ -1,17 +1,50 @@
 import React, {Component} from 'react';
 import {
     View,
-    Text,
+    StyleSheet,
+    Dimensions
 } from 'react-native';
+import Search from '../../components/Search';
 
 class MessageScreen extends Component{
+
+    gotoMessage = async ()=>{
+        await AsyncStorage.clear;
+        // this.props.navigation.navigate('');
+    }
+    
     render(){
         return (
             <View>
-                <Text>MessageScreen</Text>
+                <View style={styles.wrapperSearch}>
+                    <Search/>
+                </View>
+                <View>
+                    <TouchableOpacity
+                        style={{height: 50, width: 100}}
+                        onPress={this.gotoMessage}
+                    >
+                        <Text style={{fontSize: 30}}>
+                            Conversation1
+                        </Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         );
     }
 }
+
+const DEVICE_WIDTH = Dimensions.get('window').width;
+const DEVICE_HEIGHT = Dimensions.get('window').height;
+
+const styles = StyleSheet.create({
+    wrapperSearch: {
+        width: DEVICE_WIDTH,
+        // height: 40,
+        paddingBottom: 10,
+        paddingTop: 10,
+        backgroundColor: '#5a94f2',
+    }
+})
 
 export default MessageScreen;
