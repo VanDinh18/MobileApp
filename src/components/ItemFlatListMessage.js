644bd6a1493ea7df376dadcf47ab35750a1043e8
VanDinh18 <nguyendinh180299@gmail.com>
2020-04-22 16:22:35 +0700

    update sendmessage

---
+++ src/components/ItemFlatListMessage.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import {
+    View,
+    Text,
+    Dimensions,
+    TouchableOpacity,
+} from 'react-native';
+import User from './User';
+
+const DEVICE_WIDTH = Dimensions.get('window').width;
+const DEVICE_HEIGHT = Dimensions.get('window').height;
+
+class ItemFlatListMessage extends Component {
+
+    convertTime = (time) => {
+        let d = new Date(time);
+        let c = new Date();
+        let result = (d.getHours() < 10 ? '0' : '') + d.getHours() + ':';
+        result += (d.getMinutes() < 10 ? '0' : '') + d.getMinutes();
+        if (c.getDay() !== d.getDay()) {
+            result = d.getDay() + ' ' + d.getMonth() + ' ' + result;
+        }
+        return result;
+    }
+    render() {
+        const { item } = this.props;
+        return (
+            <View
+                style={{
+                    flexDirection: 'column',
+                    borderRadius: 10,
+                    borderWidth: 0.5,
+                    borderColor: '#d9d9d9',
+                    alignSelf: item.from === User.username ? 'flex-end' : 'flex-start',
+                    backgroundColor: item.from === User.username ? '#cce6ff' : 'white',
+                    marginBottom: 10,
+                    marginRight: item.from === User.username ? 15 : 0,
+                    marginLeft: item.from !== User.username ? 15 : 0,
+                    maxWidth: DEVICE_WIDTH * 0.8,
+                }}
+            >
+                <View style={{ 
+                    marginLeft: 8,
+                    marginRight: 8,
+                    marginTop: 5,
+                    marginBottom: 5,
+                }}>
+                    <Text style={{ fontSize: 18 }}>{item.message}</Text>
+                    <Text style={{ fontSize: 10, color: '#8c8c8c' }}>{this.convertTime(item.time)}</Text>
+                </View>
+            </View>
+        )
+    }
+
+}
+export default ItemFlatListMessage;
--- src/screens/ChatScreen/ChatScreen.js
+++ src/screens/ChatScreen/ChatScreen.js
@@ -5,18 +5,29 @@ import {
     View,
     Text,
     TextInput,
-    TouchableOpacity
+    TouchableOpacity,
+    Image,
+    FlatList,
 } from 'react-native';
+
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 import firebase from '@react-native-firebase/app';
 import '@react-native-firebase/auth';
 import '@react-native-firebase/database';
 import User from '../../components/User';
+import img from '../../assets/images/goback.png';
+import avatarImg from '../../assets/images/avatar.jpg';
+import ItemFlatListMessage from '../../components/ItemFlatListMessage';
 
 export default class ChatScreen extends Component {
     constructor(props) {
         super(props);
         this.state = {
             textMessage: '',
+            Data: [],
+            person: {
+                name: props.navigation.state.params.name
+            },
         }
         this.sendMessage = this.sendMessage.bind(this);
         this.goback = this.goback.bind(this);
@@ -48,20 +59,73 @@ export default class ChatScreen extends Component {
         }
     }
 
+    componentDidMount() {
+        firebase.database().ref('messages').child(User.username).child(this.state.person.name)
+            .on('child_added', (value) => {
+                this.setState((prevState) => {
+                    return {
+                        Data: [...prevState.Data, value.val()]
+                    }
+                })
+            })
+    }
     render() {
-        const { params } = this.props.navigation.state;
         const { navigation } = this.props;
-
         return (
             <View style={styles.wrapper}>
-                <TouchableOpacity
-                    onPress={() => this.goback(navigation)}
-                >
-                    <Text>goback</Text>
-                </TouchableOpacity>
+
                 <View style={styles.header}>
-                    <Text style={{ fontSize: 30 }}>{params.name}</Text>
+                    <TouchableOpacity
+                        onPress={() => this.goback(navigation)}
+                        style={{
+                            flex: 1,
+                            justifyContent: 'center',
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Image
+                            style={{ height: 20, width: 20, tintColor: 'white'}}
+                            source={img}
+                        />
+                    </TouchableOpacity>
+                    <View
+                        style={{
+                            flex: 5,
+                            flexDirection: 'row',
+                            alignItems: 'center',
+
+                        }}
+                    >
+                        <View style={{ flex: 1 }}>
+                            <Image
+                                style={{height: 50, width: 50, borderRadius: 50, alignItems: 'center', justifyContent: 'center' }}
+                                source={avatarImg}
+                            />
+                        </View>
+                        <View style={{ flex: 4 }}>
+                            <Text style={{ fontSize: 24, color: 'white'}}>{this.state.person.name}</Text>
+                        </View>
+
+                    </View>
+                    <View style={{ flex: 1,}}>
+
+                    </View>
                 </View>
+
+
+                <View style={styles.bodyMessage}>
+                    <FlatList
+                        data={this.state.Data}
+                        renderItem={({ item }) => (
+                            <ItemFlatListMessage
+                                item={item}
+                            />
+                        )}
+                        keyExtractor={(item, index) => index.toString()}
+                    />
+                </View>
+
                 <View style={styles.wrapperInputMessage}>
                     <TextInput
                         style={styles.inputMessage}
@@ -70,13 +134,15 @@ export default class ChatScreen extends Component {
                         onChangeText={this.handleChange('textMessage')}
                     />
                     <TouchableOpacity
-                        onPress={() => this.sendMessage(params.name)}
+                        style={styles.buttonSend}
+                        onPress={() => this.sendMessage(this.state.person.name)}
                     >
                         <Text style={{ fontSize: 30 }}>
                             Send
                         </Text>
                     </TouchableOpacity>
                 </View>
+
             </View>
         );
     }
@@ -88,16 +154,27 @@ const DEVICE_HEIGHT = Dimensions.get('window').height;
 const styles = StyleSheet.create({
     wrapper: {
         flex: 1,
-        justifyContent: 'space-between'
     },
     header: {
-        height: 30,
-
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: '#66b3ff',
     },
-    wrapperInputMessage: {
 
+    bodyMessage: {
+        flex: 10,
+        backgroundColor: '#f2f2f2',
+    },
+
+    wrapperInputMessage: {
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: 'gray',
     },
     inputMessage: {
-
+        flex: 5
+    },
+    buttonSend: {
+        flex: 1
     }
 });
